f7ceda517962c569732753f9061877aaf5db8621
JulesNyom <julesnyom@gmail.com>
2025-08-16 21:36:48 +0200

    Added a PatientRequestDTO

--- Patient-service/src/main/java/com/pm/patientservice/controller/PatientController.java
+++ Patient-service/src/main/java/com/pm/patientservice/controller/PatientController.java
@@ -1,8 +1,27 @@
 package com.pm.patientservice.controller;
 
-import org.springframework.stereotype.Controller;
+import java.util.List;
 
-@Controller
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pm.patientservice.dto.PatientResponseDTO;
+import com.pm.patientservice.service.PatientService;
+
+@RestController
+@RequestMapping("/patients") //http://localhost:4000/patients
 public class PatientController {
-    
+    private final PatientService patientService;
+
+    public PatientController(PatientService patientService) {
+        this.patientService = patientService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PatientResponseDTO>> getPatients() {
+        List<PatientResponseDTO> patients = patientService.getPatients();
+        return ResponseEntity.ok().body(patients);
+    }
 }
